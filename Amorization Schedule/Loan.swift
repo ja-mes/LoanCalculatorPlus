77d2665c66118f2410f91d0b62b57ac8b347bfb9
James <jsbrown000@gmail.com>
2016-08-24 11:07:06 -0500

    Display correctly

--- Amorization Schedule/Loan.swift
+++ Amorization Schedule/Loan.swift
@@ -53,10 +53,10 @@ class Loan {
     
     func grabAmount() -> Double {
         if let amount = _amount {
-            
-            return amount
+            return round(100 * amount) / 100
         } else if let A = _payment_amount, var i = _interest, let n = _payments {
             i = i * 0.01 / 12
+                
             let P = (A - A * pow((i + 1), -n)) / i
             return round(100 * P) / 100
         }
@@ -65,33 +65,43 @@ class Loan {
     
     func grabInterest() -> Double {
         if let interest = _interest {
-            return interest
-        } else if let P = _payment_amount, let A = _amount, let N = _payments {
+            return round(100 * interest) / 100
+        } else if let N = _payments, let P = _payment_amount, let A = _amount {
             let q = log((1 + 1 / N)) / log(2)
+            
             let i = pow((pow((1.0 + (P/A)), (1.0/q)) - 1.0), q) - 1
+            
             return round(100 * i * 100 * 12) / 100
         }
+        
         return 0.0
     }
     
     func grabPayments() -> Double {
         if let payments = _payments {
-            return payments
+            return round(100 * payments) / 100
         }
-        else if let i = _interest, let A = _amount, let P = _payment_amount {
+        else if var i = _interest, let A  = _amount, let P = _payment_amount {
+            i = i * 0.01 / 12
+            
             let N = -(log((1-i*A/P)) / log((1 + i)))
+            
             return round(100 * N) / 100
+            
         }
+        
         return 0.0
     }
     
     func grabPaymentAmount() -> Double {
         if let payment_amount = _payment_amount {
-            return payment_amount
-        } else if let i = _interest, let A = _amount, let N = _payments {
+            return round(100 * payment_amount) / 100
+        } else if var i = _interest, let A = _amount, let N = _payments {
+            i = i * 0.01 / 12
             let P = (i*A) / (1-pow((1+i), -N))
             return round(100 * P) / 100
         }
+        
         return 0.0
     }
     
